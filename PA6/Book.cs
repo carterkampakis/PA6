c40c284fb4e72daae4e543d5083df235dc1f2e82
Aden Hunsecker <cahunsecker@crimson.ua.edu>
2019-12-07 14:36:22 -0600

    Parts 3 and 4 done

--- PA6/Book.cs
+++ PA6/Book.cs
@@ -13,9 +13,31 @@ namespace PA6
         public string genre { get; set; }
         public int copies { get; set; }
         public string author { get; set; }
-        public double isbn { get; set; }
-        public double length { get; set; }
+        public string isbn { get; set; }
+        public int length { get; set; }
+        public string cover { get; set; }
+        public string _id { get; set; }
 
+        public Book(string title, string cwid, string genre, int copies, string author, string isbn, int length, string cover, string id)
+        {
+            this.title = title;
+            this.cwid = cwid;
+            this.genre = genre;
+            this.copies = copies;
+            this.author = author;
+            this.isbn = isbn;
+            this.length = length;
+            this.cover = cover;
+            _id = id;
+        }
+        public Book()
+        {
+
+        }
+        public override string ToString()
+        {
+            return this.title;
+        }
 
     }
 }
